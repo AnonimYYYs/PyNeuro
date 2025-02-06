889f35087a6f8507b05d7aa02babe7745807401a
Danlavich <kudrenokdanil@gmail.com>
2025-02-06 12:49:15 +0300

    Implemented visualisation using networkx and matplotlib

--- neuro_lib_load.py
+++ neuro_lib_load.py
@@ -6,18 +6,25 @@ dll_path = "../NeuroLib/out/build/x64-debug/NeuroLib.dll"
 
 # Загружаем DLL
 neuro_lib = ctypes.CDLL(dll_path)
-#World_new
-neuro_lib.World_new.argtypes = []
-neuro_lib.World_new.restype = ctypes.c_void_p
-#World_delete
-neuro_lib.World_delete.argtypes = [ctypes.c_void_p]
-neuro_lib.World_delete.restype = None
-#World_createRandomWorld
-neuro_lib.World_createRandomWorld.argtypes = [ctypes.c_int, ctypes.c_int, ctypes.c_float]
-neuro_lib.World_createRandomWorld.restype = ctypes.c_void_p
-#World_printIons
-neuro_lib.World_printIons.argtypes = [ctypes.c_void_p]
-neuro_lib.World_printIons.restype = None
+
+class cSynapse(ctypes.Structure):
+    _fields_ = [("neuron1_index", ctypes.c_int), ("neuron2_index", ctypes.c_int), ("weight", ctypes.c_double)]
+
+#функции из DLL
+functions = {
+    "World_new": {"argtypes": [], "restype": ctypes.c_void_p},
+    "World_delete": {"argtypes": [ctypes.c_void_p], "restype": None},
+    "World_createRandomWorld": {"argtypes": [ctypes.c_int, ctypes.c_int, ctypes.c_float], "restype": ctypes.c_void_p},
+    "World_printIons": {"argtypes": [ctypes.c_void_p], "restype": None},
+    "World_getSynapsesSize": {"argtypes": [ctypes.c_void_p], "restype": ctypes.c_size_t},
+    "World_getSynapsesData": {"argtypes": [ctypes.c_void_p], "restype": ctypes.POINTER(cSynapse)},
+    "World_getIonsSize": {"argtypes": [ctypes.c_void_p], "restype": ctypes.c_int}
+}
+#int World_getIonsSize(World* world)
+for f_name, f_data in functions.items():
+    f = getattr(neuro_lib, f_name)
+    f.argtypes = f_data["argtypes"]
+    f.restype = f_data["restype"]
 
 class World:
     def __init__(self):
@@ -37,3 +44,17 @@ class World:
     def print_ions(self):
         neuro_lib.World_printIons(self.ptr)
 
+    def get_ions_size(self):
+        return neuro_lib.World_getIonsSize(self.ptr)
+
+    def get_synapses(self):
+        synapse_array_ptr = neuro_lib.World_getSynapsesData(self.ptr)
+
+        size = neuro_lib.World_getSynapsesSize(self.ptr)
+        synapses = []
+        #переносим синапсы
+        for i in range(size):
+            synapse = cSynapse.from_address(ctypes.addressof(synapse_array_ptr.contents) + i * ctypes.sizeof(cSynapse))
+            synapses.append(synapse)
+
+        return synapses
